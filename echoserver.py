8756beea49063642bff1e2979b9cc7f60e5d478f
Meme de la Tower <mdelatorre@users.noreply.github.com>
2014-03-18 14:05:30 -0600

    Update echoserver.py

--- echoserver.py
+++ echoserver.py
@@ -6,7 +6,7 @@ import socket
 
 HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 50007              # Arbitrary non-privileged port
-s = socket(AF_INET, SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
 conn, addr = s.accept()
