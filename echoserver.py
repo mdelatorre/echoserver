0c3dff617cab2e6ff57ecb0fddcb8c3e86f5175a
Manuel de la Torre <mdltorre@up.edu.mx>
2014-03-18 13:20:46 -0600

    First commit

---
+++ echoserver.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python27
+
+# This example is from Python LibRef
+
+import socket
+
+HOST = ''                 # Symbolic name meaning all available interfaces
+PORT = 50007              # Arbitrary non-privileged port
+s = socket(AF_INET, SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+conn, addr = s.accept()
+print 'Connected by', addr
+while 1:
+    data = conn.recv(1024)
+    if not data: break
+    conn.send(data)
+conn.close()
